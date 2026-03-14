9e042203dc34ed8f9cbefa5757ceed6dfd6687f0
yahirojeda <yahir.ojeda0906@gmail.com>
2026-03-14 13:40:20 -0600

    Practica 13
    Adelanto de la practica 13.

--- practica 13/video.js
+++ practica 13/video.js
@@ -1,22 +1,84 @@
-// Aquí ponen todo su código
+class LikeSystem {
+    constructor(){
+        this.likes = 0;
+    }
+    like() {
+        this.likes++;
+    }
+    dislike(){
+        this.likes--;
+    }
+    setLikes(value) {
+        this.likes = value;
+    }
+    getLikes() {
+        return this.likes;
+    }
+}
 
-/*
-// Al final descomenten la sección para que la prueben, no deberían tener errores usando las siguientes líneas de código
+class Comment{
+    constructor(user="", comm=""){
+        this.user = user;
+        this.comm = comm;
+        this.LikeSystem = new LikeSystem();
+    }
+    printComment() {
+        console.log(`${this.user}:${this.comm}`);
+    }
+}
+
+class Video{
+    constructor(minutes=0, seconds=0, name=""){
+        this.minutes = Math.floor(minutes / 60);
+        this.seconds = seconds%60;
+        this.name = name;
+        this.LikeSystem = new LikeSystem();
+        this.comments = [];
+    }
+    
+    getCommentsNum() {
+    console.log(this.comments.lenght);
+    }
+
+    printTime() {
+        console.log(`${this.minutes}:${this.seconds}`);
+    }
+
+    addComment(comment) {
+        this.comments.push(comment);
+    }
+
+    printComments() {
+        try {
+            for (let comment of this.comments) {
+                comment.printComment();
+            }
+        } catch (e) {
+            console.log(this.comments);
+        }
+    }
+
+    // Para LikeSystem
+    like() {
+        this.LikeSystem.like();
+    }
+
+    
+    dislike() {
+        this.LikeSystem.dislike();
+    }
+
+    
+    get likes() {
+        console.log(this.LikeSystem.getLikes());
+    }
+}
+
+const video1 = new Video(1, 30, "perritos comiendo");
+const com1 = new Comment("Mario", "El gato lo atacó");
 
 com1.printComment();
-com2.printComment();
-com3.printComment();
 console.log(`El video1 tiene ${video1.likes} likes`);
 video1.printTime();
 video1.addComment(com1);
-video1.addComment(com2);
-video1.printComments(); // va a imprimir 3 comentarios
-console.log(`El video2 tiene ${video2.likes} likes`);
-video2.printTime();
-video2.addComment(com3);
-video2.addComment(com2);
-video2.addComment(com1);
-video2.printComments(); // va a imprimir 3 comentarios
-
-
-*/
+video1.printComments();
