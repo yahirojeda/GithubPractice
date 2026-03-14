7267746e842a308102e01e24d2b4362a6548d208
yahirojeda <yahir.ojeda0906@gmail.com>
2026-03-14 12:07:39 -0600

    Practica 13
    Cree archivo html y js.

---
+++ practica 13/video.js
@@ -0,0 +1,22 @@
+// Aquí ponen todo su código
+
+/*
+// Al final descomenten la sección para que la prueben, no deberían tener errores usando las siguientes líneas de código
+
+com1.printComment();
+com2.printComment();
+com3.printComment();
+console.log(`El video1 tiene ${video1.likes} likes`);
+video1.printTime();
+video1.addComment(com1);
+video1.addComment(com2);
+video1.printComments(); // va a imprimir 3 comentarios
+console.log(`El video2 tiene ${video2.likes} likes`);
+video2.printTime();
+video2.addComment(com3);
+video2.addComment(com2);
+video2.addComment(com1);
+video2.printComments(); // va a imprimir 3 comentarios
+
+
+*/
