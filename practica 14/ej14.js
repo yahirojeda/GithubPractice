90d10ea89684a1f739e7d9fb61d5cab25c019d68
yahirojeda <yahir.ojeda0906@gmail.com>
2026-03-21 12:41:37 -0600

    Practica 14
    Practica 14 con archivo js de funciones terminado.

--- practica 14/ej14.js
+++ practica 14/ej14.js
@@ -64,42 +64,49 @@ const inventario = [
   },
 ];
 
-// Darle a las variables el valor que se solicita encima de ellas.
-// Se comentó la respuesta que les debería otorgar cada una para que comprueben.
-
 //FILTER AND MAP
 //1.-Mostrar la description de los productos sin stock
 //['1K frijol peruano']
-let sinStock;
+let sinStock = inventario.filter(p => p.stock === 0).map(p => p.description);
+
 //2.-Mostrar la description de los productos con un stock menor a 5
 //['1K de tortilla', '1L Leche sello rojo', '1K frijol peruano']
-let bajoStock;
+let bajoStock = inventario.filter(p => p.stock < 5).map(p => p.description);
+
 //3.-Mostrar el name de los productos cuyo restock es cada semana = "1w"
-// ['KLGSFL', 'FJPR1K', 'CSTCHJ']
-let weeklyRestock;
+// ['KLGSFL', 'FJPR1K', 'CSTCHJ']
+let weeklyRestock = inventario.filter(p => p.restock === "1w").map(p => p.name);
+
 //4.-Mostrar el name de los productos cuyo restock no es cada semana = "1w"
 //['TORT1K', 'BRCHIP', 'SRJL1L', 'GLPH1K']
-let notWeeklyRestock;
+let notWeeklyRestock = inventario.filter(p => p.restock !== "1w").map(p => p.name);
+
 //5.-Mostrar el id de los productos que tienen un stock máximo mayor a 15 y un precio mayor a 30
 //[5]
-let mostValuableProduct;
+let mostValuableProduct = inventario.filter(p => p.maxStock > 15 && p.price > 30).map(p => p.id);
+
 //EVERY
 //6.-Todos los productos tienen por lo menos un producto en stock?
 //false
-let inventoryHasStock;
+let inventoryHasStock = inventario.every(p => p.stock > 0);
+
 //7.-Todos los productos tienen un precio mayor a 10?
 //true
-let everythingPricy;
+let everythingPricy = inventario.every(p => p.price > 10);
+
 //SOME
 //8.-Algún producto tiene el stock completo?
 //true
-let anyFullStock;
+let anyFullStock = inventario.some(p => p.stock === p.maxStock);
+
 //9.-Algún producto tiene el stock en 0?
 //true
-let anyEmptyStock;
+let anyEmptyStock = inventario.some(p => p.stock === 0);
+
 //REDUCE
 /*10.-Iniciando el acumulador en -1 calcular cuanto dinero se ha vendido en total de todo el inventario,
 si un producto tiene 10 de stock maximo y 8 de stock significa que vendió 2.
 El resultado sería el valor del producto*2-1 por el inicio del acumulador.*/
 //2023
-let moneyEarned;
+let moneyEarned = inventario.reduce((acc, p) => 
+  (acc += p.price * (p.maxStock -p.stock)), -1);
